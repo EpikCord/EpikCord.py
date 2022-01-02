aea9cac0e94eb27226337e6439c077b6ff282231
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-01-02 17:20:06 +0000

    Make avatar optional

--- EpikCord/partials.py
+++ EpikCord/partials.py
@@ -1,10 +1,14 @@
+from typing import (
+    Optional
+)
+
 class PartialUser:
     def __init__(self, data: dict):
         self.data: dict = data
-        self.avatar: str = data["avatar"]
-        self.discriminator: str = data["discriminator"]
         self.id: str = data["id"]
         self.username: str = data["username"]
+        self.discriminator: str = data["discriminator"]
+        self.avatar: Optional[str] = data["avatar"]
 
 class PartialEmoji:
     def __init__(self, data):
