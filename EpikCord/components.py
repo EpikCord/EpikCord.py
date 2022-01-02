ea873ffdff29e18d8150286c5fc9b7b831b6d883
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-01-02 13:07:50 +0000

    Update everything

--- EpikCord/components.py
+++ EpikCord/components.py
@@ -1,38 +1,148 @@
-from .exceptions import TooManyComponents, InvalidMessageButtonStyle
+from .partials import PartialEmoji
+from .exceptions import TooManyComponents, InvalidMessageButtonStyle, CustomIdIsTooBig, InvalidArgumentType, TooManySelectMenuOptions, LabelIsTooBig
 from typing import (
-    Union
+    Union,
+    List
 )
+
+
+class MessageSelectMenuOption:
+    def __init__(self, **kwargs):
+        self.data: dict = kwargs
+        self.label: str = kwargs.get("label", "")
+        self.value: str = kwargs.get("value", "")
+        self.description: str = kwargs.get("description", "")
+        self.emoji: PartialEmoji = kwargs.get("emoji", None)
+        self.default: bool = kwargs.get("default", False)
 
 class MessageButton:
     def __init__(self):
-        self.settings = {}
+        self.settings = {
+            "type": 2,
+            "style": 1,
+            
+        }
     
     def set_label(self, label: str):
+
+        if not isinstance(label, str):
+            raise InvalidArgumentType("Label must be a string.")
+        
+        if len(label) > 80:
+            raise LabelIsTooBig("Label must be 80 characters or less.")
+            
         self.settings["label"] = label
     
     def set_style(self, style: Union[int, str]):
+        valid_styles = {
+            "PRIMARY": 1,
+            "SECONDARY": 2,
+            "SUCCESS": 3,
+            "DANGER": 4,
+            "LINK": 5
+        }
         if isinstance(style, str):
-            if style.upper() not in ["PRIMARY", "SECONDARY", "LINK", "DANGER", "SUCCESS"]:
+            if style.upper() not in valid_styles:
                 raise InvalidMessageButtonStyle("Invalid button style. Style must be one of PRIMARY, SECONDARY, LINK, DANGER, or SUCCESS.")
             self.settings["style"] = style.upper()
             
         elif isinstance(style, int):
-            if style not in range(5):
+            if style not in valid_styles.values():
                 raise InvalidMessageButtonStyle("Invalid button style. Style must be in range 1 to 5 inclusive.")
-            
+    
+    def set_emoji(self, emoji: Union[PartialEmoji, dict]):
 
+        if isinstance(emoji, dict):
+            self.settings["emoji"] = emoji
+            return
+
+        elif isinstance(emoji, PartialEmoji):
+            self.settings["emoji"] = emoji.data
+            return
+        raise InvalidArgumentType("Emoji must be a PartialEmoji or a dict that represents a PartialEmoji.")
+        
+    def set_custom_id(self, custom_id: str):
+        
+        if not isinstance(custom_id, str):
+            raise InvalidArgumentType("Custom Id must be a string.")
+        
+        elif len(custom_id) > 100:
+            raise CustomIdIsTooBig("Custom Id must be 100 characters or less.")
+
+        self.settings["custom_id"] = custom_id
+        
+    def disabled(self, disabled: bool):
+        
+        if not isinstance(disabled, bool):
+            raise InvalidArgumentType("You must provide a boolean.")
+        
+        self.settings["disabled"] = disabled
+    
+    def set_url(self, url: str):
+        
+        if not isinstance(url, str):
+            raise InvalidArgumentType("Url must be a string.")
+        
+        self.settings["url"] = url
+        self.settings["style"] = 5
+        
+        
 class MessageSelectMenu:
     def __init__(self):
-        self.settings = {"options": []}
+        self.settings = {
+            "options": [], 
+            "type": 3,
+            "min_values": 1,
+            "max_values": 1,
+            "disabled": False
+        }
+        
+    def set_custom_id(self, custom_id: str):
+        if not isinstance(custom_id, str):
+            raise InvalidArgumentType("Custom Id must be a string.")
 
+        self.settings["custom_id"] = custom_id
+    
+    def add_options(self, options: List[MessageSelectMenuOption]):
+        for option in options:
+            
+            if len(self.settings["options"] > 25):
+                raise TooManySelectMenuOptions("You can only have 25 options in a select menu.")
+            
+            self.settings["options"].append(option.data)
+        
+    def set_placeholder(self, placeholder: str):
+        if not isinstance(placeholder, str):
+            raise InvalidArgumentType("Placeholder must be a string.")
+        
+        self.settings["placeholder"] = placeholder
+    
+    def set_min_values(self, min: int):
+        if not isinstance(min, int):
+            raise InvalidArgumentType("Min must be an integer.")
+        
+        self.options["min_values"] = min
+        
+    def set_max_values(self, max: int):
+        if not isinstance(max, int):
+            raise InvalidArgumentType("Max must be an integer.")
+        
+        self.options["max_values"] = max
+    
+    def set_disabled(self, disabled: bool):
+        if not isinstance(disabled, bool):
+            raise InvalidArgumentType("You must provide a boolean.")
+        
+        self.settings["disabled"] = disabled
+    
 
 class MessageActionRow:
     def __init__(self):
-        self.current_components = []
+        self.components = []
         
-    def add_components(self, components: Union[MessageButton, MessageSelectMenu]):
+    def add_components(self, components: List[Union[MessageButton, MessageSelectMenu]]):
         buttons = 0
-        for component in self.current_components:
+        for component in self.components:
             if type(component) == MessageButton:
                 buttons += 1
             
@@ -41,4 +151,4 @@ class MessageActionRow:
             
             elif type(component) == MessageSelectMenu:
                 raise TooManyComponents("You can only have 1 select menu per row. No buttons along that select menu.")
-        self.current_components.append(component)
+        self.components.append(components.settings)
--- EpikCord/partials.py
+++ EpikCord/partials.py
@@ -5,3 +5,10 @@ class PartialUser:
         self.discriminator: str = data["discriminator"]
         self.id: str = data["id"]
         self.username: str = data["username"]
+
+class PartialEmoji:
+    def __init__(self, data):
+        self.data: dict = data
+        self.name: str = data["name"]
+        self.id: str = data["id"]
+        self.animated: bool = data["animated"]
