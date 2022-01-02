bc0531891b68d4cac2ae1a0d4ca04177fbbb8d7a
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-01-02 17:49:31 +0000

    Add Guild.me to represent Client as a Member in the guild

--- EpikCord/guild.py
+++ EpikCord/guild.py
@@ -2,13 +2,11 @@ from typing import (
     Optional
 )
 
-from .client import ClientUser
-
-from .client import Client
+from .client import ClientGuildMember, Client
 
 class Guild:
     def __init__(self, client: Client, data: dict):
-        self.cache: dict = {}
+        self.member_cache: dict = {}
         self.client = client
         self.data: dict = data
         self.id: str = data["id"]
@@ -18,12 +16,5 @@ class Guild:
         self.splash: Optional[str] = data["splash"] or None
         self.discovery_splash: Optional[str] = data["discovery_splash"] or None
         self.owner_id: str = data["owner_id"]
-        self.me = ClientUser(self.client)
-        self.me.permissions = data["permissions"]
-        
-    async def fetch_member(self, guild_id: str, member_id: str = None):
-        if(self.cache[member_id]):
-            return self.cache[member_id]
-        else:
-            data = await self.client.http.get(f"guilds/{guild_id}/members/{member_id}")
-            
+        self.me = ClientGuildMember(self.client)
+        # self.me.permissions = data["permissions"]
