c5c7277a438400894b9d88b9a5c62ee1729aa678
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-01-05 21:02:12 +0000

    Add allowedmention object and Message object

--- EpikCord/message.py
+++ EpikCord/message.py
@@ -19,6 +19,21 @@ from typing import (
 )
 from .embed import Embed
 
+class MessageActivity:
+    def __init__(self, data: dict):
+        self.type: int = data["type"]
+        self.party_id: Optional[str] = data["party_id"]
+
+class AllowedMention:
+    def __init__(self, allowed_mentions: List[str], replied_user: bool, roles: List[str], users: List[str]):
+        self.data = {}
+        self.data["parse"] = allowed_mentions
+        self.data["replied_user"] = replied_user
+        self.data["roles"] = roles
+        self.data["users"] = users
+        return self.data
+    
+
 class Message:
     def __init__(self, data: dict):
         self.id: str = data["id"]
@@ -32,19 +47,19 @@ class Message:
         self.edited_timestamp: Optional[str] = data["edited_timestamp"] or None
         self.tts: bool = data["tts"]
         self.mention_everyone: bool = data["mention_everyone"]
-        self.mentions: List[MentionedUser] = [MentionedUser(mention) for mention in data["mentions"]]
-        self.mention_roles: List[Role] = [Role(role) for role in data["mention_roles"]]
-        self.mention_channels: List[MentionedChannel] = [MentionedChannel(channel) for channel in data["mention_channels"]]
-        self.embeds: List[Embed] = [Embed(embed) for embed in data["embeds"]]
-        self.reactions: Optional[List[Reaction]] = [Reaction(reaction) for reaction in data["reactions"]]
-        self.nonce: Optional[str] = data["nonce"] or None
+        self.mentions: Optional[List[MentionedUser]] = [MentionedUser(mention) for mention in data["mentions"]] or None
+        self.mention_roles: Optional[List[int]] = data["mention_roles"] or None
+        self.mention_channels: Optional[List[MentionedChannel]] = [MentionedChannel(channel) for channel in data["mention_channels"]] or None
+        self.embeds: Optional[List[Embed]] = [Embed(embed) for embed in data["embeds"]] or None
+        self.reactions: Optional[List[Reaction]] = [Reaction(reaction) for reaction in data["reactions"]] or None
+        self.nonce: Optional[Union[int, str]] = data["nonce"] or None
         self.pinned: bool = data["pinned"]
         self.type: int = data["type"]
-        self.activity: int = data["activity"]
+        self.activity: MessageActivity = MessageActivity(data["activity"])
         self.application: Application = Application(data["application"]) # Despite there being a PartialApplication, Discord don't specify what attributes it has
         self.flags: int = data["flags"]
         self.referenced_message: Optional[Message] = Message(data["referenced_message"]) if data["referenced_message"] else None
         self.interaction: Optional[MessageInteraction] = MessageInteraction(data["interaction"]) if data["interaction"] else None
         self.thread: Thread = Thread(data["thread"]) if data["thread"] else None
-        self.components: List[Union[MessageSelectMenu, MessageButton]] = [MessageSelectMenu(component) if component["type"] == 1 else MessageButton(component) for component in data["components"]]
-        self.stickers: List[StickerItem] = [StickerItem(sticker) for sticker in data["stickers"]]
+        self.components: Optional[List[Union[MessageSelectMenu, MessageButton]]] = [MessageSelectMenu(component) if component["type"] == 1 else MessageButton(component) for component in data["components"]] or None
+        self.stickers: Optional[List[StickerItem]] = [StickerItem(sticker) for sticker in data["stickers"]] or None
