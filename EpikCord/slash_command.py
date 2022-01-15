1249aa0ba272adbf1962d66c5093b86b0eba86c2
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-01-15 23:13:21 +0000

    Add Slash COmmand Object

--- EpikCord/slash_command.py
+++ EpikCord/slash_command.py
@@ -1,9 +1,71 @@
 from typing import (
-    Optional
+    Optional,
+    List,
+    Union
 )
+from .abc import BaseSlashCommandOption
+from .application import ApplicationCommand
 
-class SlashCommand: # Somethin like Command from commands.ext dpy
+class Subcommand(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 1
+        
+class SubCommandGroup(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 2
+        
+class StringOption(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 3
+        
+class IntegerOption(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 4
+        
+class BooleanOption(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 5
+        
+class UserOption(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 6
+        
+class ChannelOption(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 7
+        
+class RoleOption(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 8
+
+class MentionableOption(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 9
+    
+class NumberOption(BaseSlashCommandOption):
+    def __init__(self, *, name: str, description: Optional[str] = None, required: bool = False):
+        super().__init__(name=name, description=description, required=required)
+        self.settings["type"] = 10
+
+class SlashCommandOptionChoice:
+    def __init__(self, * name: str, value: Union[float, int, str]):
+        self.settings = {
+            "name": name,
+            "value": value
+        }
+
+
+class SlashCommand(ApplicationCommand):
     def __init__(self, data: dict):
-        self.name: str = data["name"]
-        self.description: Optional[str] = data["description"]
-        self.options
+        super().__init__(data)
+        self.options: Optional[List[Union[Subcommand, SubCommandGroup, StringOption, IntegerOption, BooleanOption, UserOption, ChannelOption, RoleOption, MentionableOption, NumberOption]]] = data["options"] or None # Return the type hinted class later this will take too long and is very tedious, I'll probably get Copilot to do it for me lmao
+
