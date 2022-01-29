7c45f4e202d05ddd9bc130661d0fe69fcd207013
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-01-29 12:35:13 +0000

    Current state

--- EpikCord/invite.py
+++ EpikCord/invite.py
@@ -1,18 +1,17 @@
 from typing import (
     Optional
 )
-from .channels import GuildStageChannel
+from .channels import GuildStageChannel, GuildChannel
 from .guild import GuildScheduledEvent
 from .application import Application
 from .user import User
-from .abc import BaseChannel
 from .partials import PartialGuild
 
 class Invite:
     def __init__(self, data: dict):
         self.code: str = data["code"]
         self.guild: Optional[PartialGuild] = PartialGuild(data["guild"]) or None
-        self.channel: BaseChannel = BaseChannel(data["channel"]) 
+        self.channel: GuildChannel = GuildChannel(data["channel"]) 
         self.inviter: Optional[User] = User(data["inviter"]) or None
         self.target_type: int = data["target_type"]
         self.target_user: Optional[User] = User(data["target_user"]) or None
@@ -23,3 +22,4 @@ class Invite:
         self.stage_instance: Optional[GuildStageChannel] = GuildStageChannel(data["stage_instance"]) or None
         self.guild_scheduled_event: Optional[GuildScheduledEvent] = GuildScheduledEvent(data["guild_scheduled_event"]) or None
     # Dabmaster is gonna work on this
+    
--- docs/discord.md
+++
@@ -1,1 +0,0 @@
-<!--someone else do this-->
