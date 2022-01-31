967e3818980fd09901220b76199a126546c9b8c6
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-01-31 21:19:46 +0000

    Remove Routes, just use the http class

--- EpikCord/route.py
+++
@@ -1,8 +0,0 @@
-class Route:
-    def __init__(self, client,url: str):
-        self.client = client
-        self.base_url = "https://discord.com/api/v9/"
-        self.url = f"{self.base_url}{url}"
-    
-    async def request(self, method: str, *args, **kwargs):
-        return await self.client.http.request(method, self.url, *args, **kwargs)
