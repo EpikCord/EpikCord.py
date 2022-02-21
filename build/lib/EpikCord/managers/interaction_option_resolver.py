ef905b84c2bb91bdf7e8b344278556fcb7dd81f5
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-02-21 10:11:08 +0800

    Delete pybiuld(its useless)

--- build/lib/EpikCord/managers/interaction_option_resolver.py
+++
@@ -1,9 +0,0 @@
-# Inspired by Discord.JS
-
-class InteractionOptionsResolver:
-    def __init__(self, option_data: dict):
-        self.data: dict = option_data # In case we miss anything and people can just do it themselves
-        self.users: dict = self.data["users"]
-        self.users: dict = self.data["members"]
-        self.roles: dict = self.data["roles"]
-        self.channels: dict = self.data["channels"]
