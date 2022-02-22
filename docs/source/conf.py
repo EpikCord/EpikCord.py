ba897f0696f96e6ed419162c629b5e2616d779f8
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-02-22 10:09:36 +0800

    actually add support

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,10 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+source_suffix = {
+    '.rst': 'resturcturedtext',
+    '.md' : 'markdown',
+}
 
 # -- Options for HTML output -------------------------------------------------
 
