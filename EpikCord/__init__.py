02373e56972bde2d249e09066f4837959f2f6df5
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-02-22 17:10:13 +0000

    Add client attribute to GuildManager

--- EpikCord/__init__.py
+++ EpikCord/__init__.py
@@ -1536,7 +1536,7 @@ class Client(WebsocketClient):
         super().__init__(token, intents)
 
         self.commands: List[dict] = [] # TODO: Need to change this to a Class Later
-        self.guilds: GuildManager = GuildManager()
+        self.guilds: GuildManager = GuildManager(self)
 
         self.http = HTTPClient(
             # raise_for_status = True,
@@ -2361,6 +2361,24 @@ class WelcomeScreen:
         self.welcome_channels: List[WelcomeChannel] = [WelcomeChannel(
             welcome_channel) for welcome_channel in data.get("welcome_channels")]
 
+class GuildScheduledEvent:
+    def __init__(self, client: Client, data: dict):
+        self.id: str = data.get("id")
+        self.client = client
+        self.guild_id: str = data.get("guild_id")
+        self.channel_id: Optional[str] = data.get("channel_id")
+        self.creator_id: Optional[str] = data.get("creator_id")
+        self.name: str = data.get("name")
+        self.description: Optional[str] = data.get("description")
+        self.scheduled_start_time: str = data.get("scheduled_start_time")
+        self.scheduled_end_time: Optional[str] = data.get("scheduled_end_time")
+        self.privacy_level: int = data.get("privacy_level")
+        self.status: str = "SCHEDULED" if data.get("status") == 1 else "ACTIVE" if data.get("status") == 2 else "COMPLETED" if data.get("status") == 3 else "CANCELLED"
+        self.entity_type: str = "STAGE_INSTANCE" if data.get("entity_type") == 1 else "VOICE" if data.get("entity_type") == 2 else "EXTERNAL"
+        self.entity_id: str = data.get("entity_id")
+        self.entity_metadata: dict = data.get("entity_metadata")
+        self.creator: Optional[User] = User(data.get("creator"))
+        self.user_count: Optional[int] = data.get("user_count")
 
 class Guild:
     def __init__(self, client: Client, data: dict):
@@ -2376,18 +2394,13 @@ class Guild:
         self.permissions: str = data.get("permissions")
         self.afk_channel_id: str = data.get("afk_channel_id")
         self.afk_timeout: int = data.get("afk_timeout")
-        self.verification_level: str = "NONE" if data.get("verification_level") == 0 else "LOW" if data.get(
-            "verification_level") == 1 else "MEDIUM" if data.get("verification_level") == 2 else "HIGH" if data.get("verification_level") == 3 else "VERY_HIGH"
-        self.default_message_notifications: str = "ALL" if data.get(
-            "default_message_notifications") == 0 else "MENTIONS"
-        self.explicit_content_filter: str = "DISABLED" if data.get(
-            "explicit_content_filter") == 0 else "MEMBERS_WITHOUT_ROLES" if data.get("explicit_content_filter") == 1 else "ALL_MEMBERS"
+        self.verification_level: str = "NONE" if data.get("verification_level") == 0 else "LOW" if data.get("verification_level") == 1 else "MEDIUM" if data.get("verification_level") == 2 else "HIGH" if data.get("verification_level") == 3 else "VERY_HIGH"
+        self.default_message_notifications: str = "ALL" if data.get("default_message_notifications") == 0 else "MENTIONS"
+        self.explicit_content_filter: str = "DISABLED" if data.get("explicit_content_filter") == 0 else "MEMBERS_WITHOUT_ROLES" if data.get("explicit_content_filter") == 1 else "ALL_MEMBERS"
         self.roles: List[Role] = [Role(role) for role in data.get("roles")]
-        self.emojis: List[Emoji] = [Emoji(emoji)
-                                    for emoji in data.get("emojis")]
+        self.emojis: List[Emoji] = [Emoji(emoji) for emoji in data.get("emojis")]
         self.features: List[str] = data.get("features")
-        self.mfa_level: str = "NONE" if data.get(
-            "mfa_level") == 0 else "ELEVATED"
+        self.mfa_level: str = "NONE" if data.get("mfa_level") == 0 else "ELEVATED"
         self.application_id: Optional[str] = data.get("application_id")
         self.system_channel_id: Optional[str] = data.get("system_channel_id")
         self.system_channel_flags: int = data.get("system_channel_flags")
@@ -2397,12 +2410,9 @@ class Guild:
         self.unavailable: bool = data.get("unavailable")
         self.member_count: int = data.get("member_count")
         # self.voice_states: List[dict] = data["voice_states"]
-        self.members: List[GuildMember] = [GuildMember(
-            member) for member in data.get("members")]
-        self.channels: List[GuildChannel] = [GuildChannel(
-            channel) for channel in data.get("channels")]
-        self.threads: List[Thread] = [
-            Thread(thread) for thread in data.get("threads")]
+        self.members: List[GuildMember] = [GuildMember(member) for member in data.get("members")]
+        self.channels: List[GuildChannel] = [GuildChannel(channel) for channel in data.get("channels")]
+        self.threads: List[Thread] = [Thread(thread) for thread in data.get("threads")]
         self.presences: List[dict] = data.get("presences")
         self.max_presences: int = data.get("max_presences")
         self.max_members: int = data.get("max_members")
@@ -2410,46 +2420,18 @@ class Guild:
         self.description: Optional[str] = data.get("description")
         self.banner: Optional[str] = data.get("banner")
         self.premium_tier: int = data.get("premium_tier")
-        self.premium_subscription_count: int = data.get(
-            "premium_subscription_count")
+        self.premium_subscription_count: int = data.get("premium_subscription_count")
         self.preferred_locale: str = data.get("preferred_locale")
-        self.public_updates_channel_id: Optional[str] = data.get(
-            "public_updates_channel_id")
-        self.max_video_channel_users: Optional[int] = data.get(
-            "max_video_channel_users")
-        self.approximate_member_count: Optional[int] = data.get(
-            "approximate_member_count")
-        self.approximate_presence_count: Optional[int] = data.get(
-            "approximate_presence_count")
-        self.welcome_screen: Optional[WelcomeScreen] = WelcomeScreen(
-            data.get("welcome_screen")) if data.get("welcome_screen") else None
+        self.public_updates_channel_id: Optional[str] = data.get("public_updates_channel_id")
+        self.max_video_channel_users: Optional[int] = data.get("max_video_channel_users")
+        self.approximate_member_count: Optional[int] = data.get("approximate_member_count")
+        self.approximate_presence_count: Optional[int] = data.get("approximate_presence_count")
+        self.welcome_screen: Optional[WelcomeScreen] = WelcomeScreen(data.get("welcome_screen")) if data.get("welcome_screen") else None
         self.nsfw_level: int = data.get("nsfw_level")
-        self.stage_instances: List[GuildStageChannel] = [
-            GuildStageChannel(channel) for channel in data.get("stage_instances")]
-        self.stickers: Optional[StickerItem] = StickerItem(
-            data.get("stickers")) if data.get("stickers") else None
+        self.stage_instances: List[GuildStageChannel] = [GuildStageChannel(channel) for channel in data.get("stage_instances")]
+        self.stickers: Optional[StickerItem] = StickerItem(data.get("stickers")) if data.get("stickers") else None
+        self.guild_schedulded_events: List[GuildScheduldedEvent] = [GuildScheduldedEvent(event) for event in data.get("guild_schedulded_events", [])]
 
-
-class GuildScheduledEvent:
-    def __init__(self, client: Client, data: dict):
-        self.id: str = data.get("id")
-        self.client = client
-        self.guild_id: str = data.get("guild_id")
-        self.channel_id: Optional[str] = data.get("channel_id")
-        self.creator_id: Optional[str] = data.get("creator_id")
-        self.name: str = data.get("name")
-        self.description: Optional[str] = data.get("description")
-        self.scheduled_start_time: str = data.get("scheduled_start_time")
-        self.scheduled_end_time: Optional[str] = data.get("scheduled_end_time")
-        self.privacy_level: int = data.get("privacy_level")
-        self.status: str = "SCHEDULED" if data.get("status") == 1 else "ACTIVE" if data.get(
-            "status") == 2 else "COMPLETED" if data.get("status") == 3 else "CANCELLED"
-        self.entity_type: str = "STAGE_INSTANCE" if data.get(
-            "entity_type") == 1 else "VOICE" if data.get("entity_type") == 2 else "EXTERNAL"
-        self.entity_id: str = data.get("entity_id")
-        self.entity_metadata: dict = data.get("entity_metadata")
-        self.creator: Optional[User] = User(data.get("creator"))
-        self.user_count: Optional[int] = data.get("user_count")
 
 
 class WebhookUser:
@@ -2819,7 +2801,7 @@ class Team:
         self.id: str = data.get("id")
         self.members: List[TeamMember] = data.get("members")
 
-class ClientUser():
+class ClientUser:
 
     def __init__(self, client, data: dict):
         self.client = client
--- EpikCord/managers/guildsmanager.py
+++ EpikCord/managers/guildsmanager.py
@@ -8,7 +8,12 @@ from ..__init__ import Guild, UnavailableGuild
 
 
 class GuildManager(CacheManager):
-    def __init__(self, guilds: Optional[List[Union[Guild, UnavailableGuild]]] = []):
+    def __init__(self, client, guilds: Optional[List[Union[Guild, UnavailableGuild]]] = []):
         super().__init__()
+        self.client = client
         for guild in guilds:
             self.cache[guild.id] = guild
+
+    async def fetch(self, guild_id: str, *, skip_cache: Optional[bool] = False, with_counts: Optional[bool] = False):
+        if skip_cache:
+            res = await self.client.http.get(f"/guilds/{guild_id}")
