98a12ab02c0e31cdc3fd7e67666a485ec63910bf
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-03-07 17:23:59 +0000

    Remove Danny's license on the code he didn't work on

--- EpikCord/managers/__init__.py
+++ EpikCord/managers/__init__.py
@@ -1,7 +1,4 @@
 """
-Some parts of the code is done by discord.py and their amazing team of contributors
-The MIT License (MIT)
-Copyright © 2015-2021 Rapptz
 Copyright © 2021-present EpikHost
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
