8067671c4b67e1cd0f68a858c31f2b288e6e712d
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-03-08 15:37:05 +0800

    Fix spelling

--- EpikCord/ext/shell.py
+++ EpikCord/ext/shell.py
@@ -23,7 +23,7 @@ class ShellHandler:
         self.shell:List[str] = shells[self.system]
         self.owners = client.is_owner() if client.is_owner is not [] else owner_ids
         if self.owners == []:
-            raise CWE78Error("As a security measure, we are preventing you to do this without owner ids.\nThis is because allowing could make arbitary code exection possible")
+            raise CWE78Error("As a security measure, we are preventing you to do this without owner ids.\nThis is because allowing could make arbitrary code execution possible")
         LogHandler.info("Finding Terminal!")
         if self.system == "windows":
             for shell in self.shell:
