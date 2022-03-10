9029b6fb5ad70c484647d35ae8bc51888095a3a1
Jerry.py <Theofficialjerrypy@gmail.com>
2022-03-10 16:54:13 -0500

    Update

--- EpikCord/application.py
+++ EpikCord/application.py
@@ -1,5 +1,4 @@
 import datetime
-from .application import Application
 from .errors import InvalidApplicationCommandType, InvalidApplicationCommandOptionType
 from .member import User, PartialUser
 from .slash import (
@@ -15,9 +14,40 @@ from .slash import (
     MentionableOption, 
     NumberOption
 )
-from .team import Team
 from typing import Optional, List
 
+class Application:
+    def __init__(self, data: dict):
+        self.id: str = data.get("id")
+        self.name: str = data.get("name")
+        self.icon: Optional[str] = data.get("icon")
+        self.description: str = data.get("description")
+        self.rpc_origins: Optional[list] = data.get("rpc_origins")
+        self.bot_public: bool = data.get("bot_public")
+        self.bot_require_code_grant: bool = data.get("bot_require_code_grant")
+        self.terms_of_service_url: Optional[str] = data.get("terms_of_service")
+        self.privacy_policy_url: Optional[str] = data.get("privacy_policy")
+        self.owner: Optional[PartialUser] = PartialUser(data.get("user")) if data.get("user") else None
+        self.summary: str = data.get("summary")
+        self.verify_key: str = data.get("verify_key")
+        self.team: Optional[Team] = Team(data.get("team")) if data.get("get") else None
+        self.cover_image: Optional[str] = data.get("cover_image")
+        self.flags: int = data.get("flags")
+
+class TeamMember:
+    def __init__(self, data: dict):
+        self.data = data
+        self.membership_state: int = data.get("membership_state")
+        self.team_id: str = data.get("team_id")
+        self.user: PartialUser = PartialUser(data.get("user"))
+
+
+class Team:
+    def __init__(self, data: dict):
+        self.data = data
+        self.icon: str = data.get("icon")
+        self.id: str = data.get("id")
+        self.members: List[TeamMember] = data.get("members")
 
 class ApplicationCommandPermission:
     def __init__(self, data: dict):
@@ -194,22 +224,3 @@ class ClientApplication(Application):
     async def edit_application_command_permissions(self, guild_id: str, command_id, *, permissions: List[ApplicationCommandPermission]):
         payload = [permission.to_dict() for permission in permissions]
         await self.client.http.put(f"/applications/{self.id}/guilds/{guild_id}/commands/{command_id}/permissions", json = payload)
-
-
-class Application:
-    def __init__(self, data: dict):
-        self.id: str = data.get("id")
-        self.name: str = data.get("name")
-        self.icon: Optional[str] = data.get("icon")
-        self.description: str = data.get("description")
-        self.rpc_origins: Optional[list] = data.get("rpc_origins")
-        self.bot_public: bool = data.get("bot_public")
-        self.bot_require_code_grant: bool = data.get("bot_require_code_grant")
-        self.terms_of_service_url: Optional[str] = data.get("terms_of_service")
-        self.privacy_policy_url: Optional[str] = data.get("privacy_policy")
-        self.owner: Optional[PartialUser] = PartialUser(data.get("user")) if data.get("user") else None
-        self.summary: str = data.get("summary")
-        self.verify_key: str = data.get("verify_key")
-        self.team: Optional[Team] = Team(data.get("team")) if data.get("get") else None
-        self.cover_image: Optional[str] = data.get("cover_image")
-        self.flags: int = data.get("flags")
