553a2d38c40316b22858188fc2d3f5366859741b
Jerry.py <Theofficialjerrypy@gmail.com>
2022-03-10 15:49:52 -0500

    Add all

---
+++ EpikCord/role.py
@@ -0,0 +1,25 @@
+from typing import Optional
+
+class RoleTag:
+    def __init__(self, data: dict):
+        self.bot_id: Optional[str] = data.get("bot_id")
+        self.integration_id: Optional[str] = data.get("integration_id")
+        self.premium_subscriber: Optional[bool] = data.get(
+            "premium_subscriber")
+
+
+class Role:
+    def __init__(self, client, data: dict):
+        self.data = data
+        self.client = client
+        self.id: str = data.get("id")
+        self.name: str = data.get("name")
+        self.color: int = data.get("color")
+        self.hoist: bool = data.get("hoist")
+        self.icon: Optional[str] = data.get("icon")
+        self.unicode_emoji: Optional[str] = data.get("unicode_emoji")
+        self.position: int = data.get("position")
+        self.permissions: str = data.get("permissions")  # TODO: Permissions
+        self.managed: bool = data.get("managed")
+        self.mentionable: bool = data.get("mentionable")
+        self.tags: RoleTag = RoleTag(self.data.get("tags"))
