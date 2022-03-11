d6b66a84d7be315002860933854f97199f70ae11
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-03-11 17:12:40 +0000

    Remove repeated code

--- EpikCord/sticker.py
+++ EpikCord/sticker.py
@@ -6,13 +6,10 @@ class StickerItem:
 
 class Sticker(StickerItem):
     def __init__(self, data: dict):
-        super().__init__(data=data)
-        self.id: str = data.get("id")
-        self.name: str = data.get("name")
+        super().__init__(data)
         self.description: str = data.get("description")
         self.tags: str = data.get("tags")
         self.type: str = data.get("image")
-        self.format_type: int = data.get("format_type")
         self.pack_id: int = data.get("pack_id")
         self.sort_value: int = data.get("sort_value")
 
