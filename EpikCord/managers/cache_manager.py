a1a3bb517c5953852b09544eec707041e51f6d88
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-03-24 17:41:51 +0000

    Refactor CacheManager class

--- EpikCord/managers/cache_manager.py
+++ EpikCord/managers/cache_manager.py
@@ -1,77 +1,78 @@
 class CacheManager:
-    def __init__(self):
-        self.cache: dict = {}
+    def __init__(self, cache_key: str = "cache"):
+        self.cache_key: str = cache_key
+        setattr(self, cache_key, []) # Dynamic for subclasses
     
     def add_to_cache(self, key, value):
-        self.cache[key] = value
+        getattr(self, self.cache_key)[key] = value
     
     def remove_from_cache(self, key):
-        self.cache.pop(key, None)
+        getattr(self, self.cache_key).pop(key, None)
         
     def get_from_cache(self, key):
-        return self.cache.get(key, None)
+        return getattr(self, self.cache_key).get(key, None)
 
     def is_in_cache(self, key):
-        return key in self.cache
+        return key in getattr(self, self.cache_key)
     
     def clear_cache(self):
-        self.cache = {}
+        setattr(self, self.cache_key, {})
     
     def __dict__(self):
-        return self.cache
+        return getattr(self, self.cache_key)
     
     def __str__(self):
-        return f"<CacheManager cache={self.cache}>"
+        return f"<CacheManager cache={getattr(self, self.cache_key)}>"
 
     def __int__(self):
-        return len(self.cache)
+        return len(getattr(self, self.cache_key))
     
     def __len__(self):
-        return len(self.cache)
+        return len(getattr(self, self.cache_key))
     
     def __repr__(self):
         return self.__str__()
     
     def __getitem__(self, key):
-        return self.cache[key]
+        return getattr(self, self.cache_key)[key]
     
     def __setitem__(self, key, value):
-        self.cache[key] = value
+        getattr(self, self.cache_key)[key] = value
     
     def __delitem__(self, key):
-        self.cache.pop(key, None)
+        getattr(self, self.cache_key).pop(key, None)
     
     def __contains__(self, key):
-        return key in self.cache
+        return key in getattr(self, self.cache_key)
     
     def __iter__(self):
-        return iter(self.cache)
+        return iter(getattr(self, self.cache_key))
     
     def __next__(self):
-        return next(self.cache)
+        return next(getattr(self, self.cache_key))
     
     def __eq__(self, other):
-        return self.cache == other.cache
+        return getattr(self, self.cache_key) == other.cache
     
     def __ne__(self, other):
-        return self.cache != other.cache
+        return getattr(self, self.cache_key) != other.cache
     
     def __gt__(self, other):
-        return self.cache > other.cache
+        return getattr(self, self.cache_key) > other.cache
     
     def __ge__(self, other):
-        return self.cache >= other.cache
+        return getattr(self, self.cache_key) >= other.cache
     
     def __lt__(self, other):
-        return self.cache < other.cache
+        return getattr(self, self.cache_key) < other.cache
     
     def __le__(self, other):
-        return self.cache <= other.cache
+        return getattr(self, self.cache_key) <= other.cache
     
     def __hash__(self):
-        return hash(self.cache)
+        return hash(getattr(self, self.cache_key))
     
     def __call__(self):
-        return self.cache
+        return getattr(self, self.cache_key)
     
 # This is the base cache manager, people can extend this to make their own cache managers
