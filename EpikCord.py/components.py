7ce2f0405492ce4b8d343bafe14cf5ba6f3cfe7e
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-04-02 10:44:44 +0100

    Adhere to self.settings being removed... which was 2 months ago...

--- EpikCord.py/components.py
+++ EpikCord.py/components.py
@@ -17,15 +17,21 @@ class BaseComponent:
     custom_id: :class:`str`
         The custom id of the component. Used for `Client.component(custom_id)`.
     
-    Methods
-    -------
-    set_custom_id(custom_id: :class:`str`)
-        Sets the custom id of the component.
     """
     def __init__(self, *, custom_id: str):
         self.custom_id: str = custom_id
 
     def set_custom_id(self, custom_id: str):
+        """Sets the custom id of the component.
+
+        Parameters:
+            custom_id: :class:`str`
+                The custom id of the component.
+
+        Raises:
+            InvalidArgumentType: If the Custom_ID is not a string.
+            CustomIdIsTooBig: If the Custom_ID is bigger than 100 characters.
+        """
 
         if not isinstance(custom_id, str):
             raise InvalidArgumentType("Custom Id must be a string.")
@@ -37,6 +43,37 @@ class BaseComponent:
 
 
 class SelectMenuOption:
+    """
+    A class representation of a select menu option.
+    This is for you to add to a ``SelectMenu`` via `SeletMenu.add_options`.
+
+    Attributes
+    ----------
+    label: :class:`str`
+        The label of the option.
+    value: :class:`str`
+        The value of the option.
+    description: :class:`str`
+        The description of the option.
+    emoji: :class:`PartialEmoji`
+        The emoji of the option.
+    default: :class:`bool`
+        Wether this option is added by default.
+
+    Parameters
+    ----------
+    label: :class:`str`
+        The label of the option.
+    value: :class:`str`
+        The value of the option.
+    description: :class:`str`
+        The description of the option.
+    emoji: :class:`PartialEmoji`
+        The emoji of the option.
+    default: :class:`bool`
+        Wether this option is added by default.
+
+    """
     def __init__(self, label: str, value: str, description: Optional[str] = None, emoji: Optional[PartialEmoji] = None, default: Optional[bool] = None):
         self.label: str = label
         self.value: str = value
@@ -45,6 +82,9 @@ class SelectMenuOption:
         self.default: Optional[bool] = default
 
     def to_dict(self):
+        """
+        Returns a dict representation of the option.
+        """
         settings = {
             "label": self.label,
             "value": self.value
@@ -67,16 +107,45 @@ class SelectMenuOption:
 
 
 class SelectMenu(BaseComponent):
-    def __init__(self, *, min_values: Optional[int] = 1, max_values: Optional[int] = 1, disabled: Optional[bool] = False, custom_id: str):
+    """
+    A class representation of a select menu.
+    
+    Attributes
+    ----------
+    min_values: :class:`int`
+        The minimum amount of values that can be selected.
+    max_values: :class:`int`
+        The maximum amount of values that can be selected.
+    disabled: :class:`bool`
+        Wether the select menu is disabled.
+    custom_id: :class:`str`
+        The custom id of the select menu. Used for `Client.component(custom_id)`.
+    
+    Parameters
+    ----------
+    min_values: :class:`int`
+        The minimum amount of values that can be selected.
+    max_values: :class:`int`
+        The maximum amount of values that can be selected.
+    disabled: :class:`bool`
+        Wether the select menu is disabled.
+    custom_id: :class:`str`
+        The custom id of the select menu. Used for `Client.component(custom_id)`.
+    """
+    def __init__(self, *, min_values: Optional[int] = 1, max_values: Optional[int] = 1, disabled: Optional[bool] = False, custom_id: str, placeholder: Optional[str] = None):
         super().__init__(custom_id=custom_id)
         self.options: List[Union[SelectMenuOption, dict]] = []
         self.type: str = 3
+        self.placeholder = placeholder
         self.min_values = min_values
         self.max_values = max_values
         self.disabled: bool = disabled
 
     def to_dict(self):
-        return {
+        """
+        Returns a dict representation of the select menu.
+        """
+        payload = {
             "type": self.type,
             "options": self.options,
             "min_values": self.min_values,
@@ -85,7 +154,23 @@ class SelectMenu(BaseComponent):
             "custom_id": self.custom_id
         }
 
+        if self.placeholder:
+            payload["placeholder"] = self.placeholder
+        
+        return payload
+
     def add_options(self, options: List[SelectMenuOption]):
+        """
+        Adds options to the select menu.
+
+        Parameters:
+            options: :class:`List[SelectMenuOption]`
+                The options to add.
+
+        Raises:
+            TooManySelectMenuOptions: If you reach 26 > options.
+
+        """
         for option in options:
 
             if len(self.options) > 25:
@@ -96,47 +181,98 @@ class SelectMenu(BaseComponent):
         return self
 
     def set_placeholder(self, placeholder: str):
+        """
+        Sets the placeholder of the select menu.
+
+        Parameters:
+            placeholder: :class:`str`
+                The placeholder of the select menu.
+        
+        Raises:
+            InvalidArgumentType: If the placeholder is not a string.
+        """
+
         if not isinstance(placeholder, str):
             raise InvalidArgumentType("Placeholder must be a string.")
 
-        self.settings["placeholder"] = placeholder
+        self.placeholder = placeholder
         return self
 
     def set_min_values(self, min: int):
+        """
+        Set the minimum amount of values that can be selected.
+
+        Parameters:
+            min: :class:`int`
+                The minimum amount of values that can be selected.
+
+        Raises:
+            InvalidArgumentType: If the min is not an integer.
+        """
         if not isinstance(min, int):
             raise InvalidArgumentType("Min must be an integer.")
 
-        self.options["min_values"] = min
+        self.min_values = min
         return self
 
     def set_max_values(self, max: int):
+        """
+        """
         if not isinstance(max, int):
             raise InvalidArgumentType("Max must be an integer.")
 
-        self.options["max_values"] = max
+        self.max_values = max
         return self
 
     def set_disabled(self, disabled: bool):
+        """
+        Sets the disabled state of the select menu.
+
+        Parameters:
+            disabled: :class:`bool`
+        
+        Raises:
+            InvalidArgumentType: If the disabled is not a boolean.
+        """
+        if not isinstance(disabled, bool):
+            raise InvalidArgumentType("Disabled must be a boolean.")
         self.disabled = disabled
 
 
 class TextInput(BaseComponent):
+    """
+    A class representation of a text input.
+    Can only be used in a :class:`Modal`.
+
+    Attributes
+    ----------
+    placeholder: :class:`str`
+        The placeholder of the text input.
+    max_length: :class:`int`
+        The maximum length of the text input.
+    disabled: :class:`bool`
+        Wether the text input is disabled.
+    custom_id: :class:`str`
+        The custom id of the text input. Used for `Client.component(custom_id)`.
+    
+    """
     def __init__(self, *, custom_id: str, style: Union[int, str] = 1, label: str, min_length: Optional[int] = 1, max_length: Optional[int] = 4000, required: Optional[bool] = True, value: Optional[str] = None, placeholder: Optional[str] = None):
         super().__init__(custom_id=custom_id)
         VALID_STYLES = {
-            "Short": 1,
-            "Paragraph": 2
+            "short": 1,
+            "paragraph": 2
         }
 
         if isinstance(style, str):
-            if style not in VALID_STYLES:
-                raise InvalidComponentStyle(
-                    "Style must be either 'Short' or 'Paragraph'.")
-            style = VALID_STYLES[style]
+            if style.lower() not in VALID_STYLES:
+                raise InvalidComponentStyle("Style must be either 'Short' or 'Paragraph'.")
+            style = VALID_STYLES[style.lower()]
 
         elif isinstance(style, int):
             if style not in VALID_STYLES.values():
                 raise InvalidComponentStyle("Style must be either 1 or 2.")
+        else:
+            raise InvalidComponentStyle("Style must be an int or str.")
 
         self.style: int = style
         self.type: int = 4
