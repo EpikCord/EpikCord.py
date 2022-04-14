5787ff6fdb484e782ec7a2da0969f98f1538fc0f
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-04-14 12:46:25 +0100

    Migrate ClientUser to user.py

---
+++ EpikCord/user.py
@@ -0,0 +1,37 @@
+from typing import Optional
+from logging import getLogger
+
+logger = getLogger(__name__)
+
+class ClientUser:
+
+    def __init__(self, client, data: dict):
+        self.client = client
+        self.data = data
+        self.verified: bool = data.get("verified")
+        self.username: str = data.get("username")
+        self.mfa_enabled: bool = data.get("mfa_enabled")
+        self.id: str = data.get("id")
+        self.flags: int = data.get("flags")
+        self.email: Optional[str] = data.get("email")
+        self.discriminator: str = data.get("discriminator")
+        self.bot: bool = data.get("bot")
+        self.avatar: str = data.get("avatar")
+        if not self.bot:  # if they're a user account
+            logger.warning("Warning: Self botting is against Discord ToS. You can get banned.")
+
+    async def fetch(self):
+        response = await self.client.http.get("users/@me")
+        data = await response.json()
+        super().__init__(data)  # Reinitialse the class with the new data.
+
+    async def edit(self, *, username: Optional[str] = None, avatar: Optional[bytes] = None):
+        payload = {}
+        if username:
+            payload["username"] = username
+        if avatar:
+            payload["avatar"] = self.client.utils._bytes_to_base64_data(avatar)
+        response = await self.client.http.patch("users/@me", json=payload)
+        data = await response.json()
+        # Reinitialize the class with the new data, the full data.
+        self.__init__(data)
