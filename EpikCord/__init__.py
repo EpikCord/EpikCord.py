d85a70e2aa0da6469af657678e1b0034e43a4098
Zeno <57388886+ZenoABC@users.noreply.github.com>
2022-04-16 17:33:39 +0300

    Use proper operation for permissions and intents

--- EpikCord/__init__.py
+++ EpikCord/__init__.py
@@ -1409,77 +1409,77 @@ class Intents:
 
     @property
     def guilds(self):
-        self.value += 1 << 0
+        self.value |= 1 << 0
         return self
 
     @property
     def guild_members(self):
-        self.value += 1 << 1
+        self.value |= 1 << 1
         return self
 
     @property
     def guild_bans(self):
-        self.value += 1 << 2
+        self.value |= 1 << 2
         return self
 
     @property
     def guild_emojis_and_stickers(self):
-        self.value += 1 << 3
+        self.value |= 1 << 3
         return self
 
     @property
     def guild_integrations(self):
-        self.value += 1 << 4
+        self.value |= 1 << 4
         return self
 
     @property
     def guild_webhooks(self):
-        self.value += 1 << 5
+        self.value |= 1 << 5
         return self
 
     @property
     def guild_invites(self):
-        self.value += 1 << 6
+        self.value |= 1 << 6
         return self
 
     @property
     def guild_voice_states(self):
-        self.value += 1 << 7
+        self.value |= 1 << 7
         return self
 
     @property
     def guild_presences(self):
-        self.value += 1 << 8
+        self.value |= 1 << 8
         return self
 
     @property
     def guild_messages(self):
-        self.value += 1 << 9
+        self.value |= 1 << 9
         return self
 
     @property
     def guild_message_reactions(self):
-        self.value += 1 << 10
+        self.value |= 1 << 10
         return self
 
     @property
     def guild_message_typing(self):
-        self.value += 1 << 11
+        self.value |= 1 << 11
         return self
 
     @property
     def direct_messages(self):
-        self.value += 1 << 12
+        self.value |= 1 << 12
         return self
 
     @property
     def direct_message_reactions(self):
-        self.value += 1 << 13
+        self.value |= 1 << 13
         return self
 
     @property
     def direct_message_typing(self):
-        self.value += 1 << 14
+        self.value |= 1 << 14
         return self
 
     @property
@@ -1512,7 +1512,7 @@ class Intents:
 
     @property
     def message_content(self):
-        self.value += 1 << 15
+        self.value |= 1 << 15
         return self
 
     # TODO: Add some presets such as "Moderation", "Logging" etc.
@@ -1524,202 +1524,202 @@ class Permission:
 
     @property
     def create_instant_invite(self):
-        self.value += 1 << 0
+        self.value |= 1 << 0
         return self
 
     @property
     def kick_members(self):
-        self.value += 1 << 1
+        self.value |= 1 << 1
         return self
 
     @property
     def ban_members(self):
-        self.value += 1 << 2
+        self.value |= 1 << 2
         return self
 
     @property
     def administrator(self):
-        self.value += 1 << 3
+        self.value |= 1 << 3
         return self
 
     @property
     def manage_channels(self):
-        self.value += 1 << 4
+        self.value |= 1 << 4
         return self
 
     @property
     def manage_guild(self):
-        self.value += 1 << 5
+        self.value |= 1 << 5
         return self
 
     @property
     def add_reactions(self):
-        self.value += 1 << 6
+        self.value |= 1 << 6
         return self
 
     @property
     def view_audit_log(self):
-        self.value += 1 << 7
+        self.value |= 1 << 7
         return self
 
     @property
     def priority_speaker(self):
-        self.value += 1 << 8
+        self.value |= 1 << 8
         return self
 
     @property
     def stream(self):
-        self.value += 1 << 9
+        self.value |= 1 << 9
         return self
 
     @property
     def read_messages(self):
-        self.value += 1 << 10
+        self.value |= 1 << 10
         return self
 
     @property
     def send_messages(self):
-        self.value += 1 << 11
+        self.value |= 1 << 11
         return self
 
     @property
     def send_tts_messages(self):
-        self.value += 1 << 12
+        self.value |= 1 << 12
         return self
 
     @property
     def manage_messages(self):
-        self.value += 1 << 13
+        self.value |= 1 << 13
         return self
 
     @property
     def embed_links(self):
-        self.value += 1 << 14
+        self.value |= 1 << 14
         return self
 
     @property
     def attach_files(self):
-        self.value += 1 << 15
+        self.value |= 1 << 15
         return self
 
     @property
     def read_message_history(self):
-        self.value += 1 << 16
+        self.value |= 1 << 16
         return self
 
     @property
     def mention_everyone(self):
-        self.value += 1 << 17
+        self.value |= 1 << 17
         return self
 
     @property
     def use_external_emojis(self):
-        self.value += 1 << 18
+        self.value |= 1 << 18
         return self
 
     @property
     def connect(self):
-        self.value += 1 << 20
+        self.value |= 1 << 20
         return self
 
     @property
     def speak(self):
-        self.value += 1 << 21
+        self.value |= 1 << 21
         return self
 
     @property
     def mute_members(self):
-        self.value += 1 << 22
+        self.value |= 1 << 22
         return self
 
     @property
     def deafen_members(self):
-        self.value += 1 << 23
+        self.value |= 1 << 23
         return self
 
     @property
     def move_members(self):
-        self.value += 1 << 24
+        self.value |= 1 << 24
         return self
 
     @property
     def use_voice_activation(self):
-        self.value += 1 << 25
+        self.value |= 1 << 25
         return self
 
     @property
     def change_nickname(self):
-        self.value += 1 << 26
+        self.value |= 1 << 26
         return self
 
     @property
     def manage_nicknames(self):
-        self.value += 1 << 27
+        self.value |= 1 << 27
         return self
 
     @property
     def manage_roles(self):
-        self.value += 1 << 28
+        self.value |= 1 << 28
         return self
 
     @property
     def manage_webhooks(self):
-        self.value += 1 << 29
+        self.value |= 1 << 29
         return self
 
     @property
     def manage_emojis_and_stickers(self):
-        self.value += 1 << 30
+        self.value |= 1 << 30
         return self
 
     @property
     def use_application_commands(self):
-        self.value += 1 << 31
+        self.value |= 1 << 31
         return self
 
     @property
     def request_to_speak(self):
-        self.value += 1 << 32
+        self.value |= 1 << 32
         return self
 
     @property
     def manage_events(self):
-        self.value += 1 << 33
+        self.value |= 1 << 33
         return self
 
     @property
     def manage_threads(self):
-        self.value += 1 << 34
+        self.value |= 1 << 34
         return self
 
     @property
     def create_public_threads(self):
-        self.value += 1 << 35
+        self.value |= 1 << 35
         return self
 
     @property
     def create_private_threads(self):
-        self.value += 1 << 36
+        self.value |= 1 << 36
         return self
 
     @property
     def use_external_stickers(self):
-        self.value += 1 << 37
+        self.value |= 1 << 37
         return self
 
     @property
     def send_messages_in_threads(self):
-        self.value += 1 << 38
+        self.value |= 1 << 38
         return self
 
     @property
     def start_embedded_activities(self):
-        self.value += 1 << 39
+        self.value |= 1 << 39
         return self
 
     @property
     def moderator_members(self):
-        self.value += 1 << 40
+        self.value |= 1 << 40
         return self
 
 class VoiceState:
