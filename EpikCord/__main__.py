5d6466c5e1f88d0b6c345fadb91939f77675e555
nav-github01001 <krishnanav309@gmail.com>
2022-05-08 18:19:20 +0800

    Version

--- EpikCord/__main__.py
+++ EpikCord/__main__.py
@@ -4,7 +4,7 @@ __all__ = (
 
 
 
-__version__= '0.4.11'
+__version__= '0.4.13.3'
 
 def info():
     print(f"Version {__version__()} of EpikCord.py, written by EpikHost. This is an unstable build and will contain bugs. Please report any bugs to https://github.com/EpikHost/EpikCord.py/issues.")
