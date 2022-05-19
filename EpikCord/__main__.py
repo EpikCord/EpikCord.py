929ffc40180ae8c95dbd135850e99eff4fe711d2
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-05-19 13:41:55 +0000

    Use new Git URL

--- EpikCord/__main__.py
+++ EpikCord/__main__.py
@@ -7,7 +7,7 @@ __all__ = (
 __version__= '0.4.13.3'
 
 def info():
-    print(f"Version {__version__()} of EpikCord.py, written by EpikHost. This is an unstable build and will contain bugs. Please report any bugs to https://github.com/EpikHost/EpikCord.py/issues.")
+    print(f"Version {__version__()} of EpikCord.py, written by EpikHost. This is an unstable build and will contain bugs. Please report any bugs to https://github.com/EpikCord/EpikCord.py/issues.")
 
 if __name__ == "__main__":
     info()
