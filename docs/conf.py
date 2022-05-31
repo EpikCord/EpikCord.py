f8320d02ed6d43e3ec0f4b2d1bfc1643b528e1b9
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-05-31 12:30:12 +0100

    New documentation.

--- docs/conf.py
+++ docs/conf.py
@@ -31,7 +31,11 @@ release = '0.4.13'
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
+    'sphinx.ext.napoleon',
 ]
+ 
+napoleon_google_docstring = False
+napoleon_use_param = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -47,7 +51,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
