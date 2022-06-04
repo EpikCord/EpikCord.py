3ccd3b354be0cad1779fb5ba33800076c884f379
sigmanificient <edhyjox@gmail.com>
2022-06-04 18:52:56 +0200

    :recycle: Rewrote Paginator

--- EpikCord/__init__.py
+++ EpikCord/__init__.py
@@ -3829,25 +3829,47 @@ class VoiceState:
 class Paginator:
     def __init__(self, *, pages: List[Embed]):
         self.current_index: int = 0
-        self.pages = pages
+        self.__pages = pages
 
-    def back(self):
-        return self.pages[self.current_index - 1]
+    def __iter__(self):
+        return self
+
+    def __len__(self):
+        return len(self.__pages)
+
+    def __next__(self):
+        return self.forward()
+
+    @property
+    def page(self):
+        return self.__pages[self.current_index]
 
     def forward(self):
-        return self.pages[self.current_index + 1]
+        self.current_index = min(len(self.__pages), self.current_index + 1)
+        return self.__pages[self.current_index]
 
-    def current(self):
-        return self.pages[self.current_index]
+    def back(self):
+        self.current_index = max(0, self.current_index - 1)
+        return self.__pages[self.current_index]
+
+    def first(self):
+        self.current_index = 0
+
+    def last(self):
+        self.current_index = len(self.__pages)
 
     def add_page(self, page: Embed):
-        self.pages.append(page)
+        self.__pages.append(page)
+
+    def insert_page(self, page: Embed, index: int):
+        if index >= len(self.__pages):
+            self.add_page(page)
+            return
+
+        self.__pages.index(page, index)
 
     def remove_page(self, page: Embed):
-        self.pages = len(filter(lambda embed: embed != page, self.pages))
-
-    def current(self) -> Embed:
-        return self.pages[self.current_index]
+        self.__pages = list(filter(lambda embed: embed != page, self.__pages))
 
 
 class Utils:
