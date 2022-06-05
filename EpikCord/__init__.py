9d4b02898090c8b0d4a8105494d81d31c26c2308
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-06-05 18:50:05 +0100

    I forgot what I changed

--- EpikCord/__init__.py
+++ EpikCord/__init__.py
@@ -2033,6 +2033,9 @@ class Event:
 
 
 class Section:
+
+    
+
     def __init_subclass__(cls, **kwargs):
         for attr_value in cls.__dict__.values():
             if isinstance(attr_value, Event):
