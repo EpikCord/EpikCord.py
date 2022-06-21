25a8a5f8005ef79990f8ed4d2c666d6a5f363271
sigmanificient <edhyjox@gmail.com>
2022-06-21 23:26:23 +0200

    :sparkles: Added implementation for DiscordAPIError
    Signed-off-by: sigmanificient <edhyjox@gmail.com>

--- EpikCord/exceptions.py
+++ EpikCord/exceptions.py
@@ -1,3 +1,6 @@
+from dataclasses import dataclass
+
+
 class EpikCordException(Exception):
     ...
 
@@ -30,8 +33,43 @@ class MissingCustomId(EpikCordException):
     ...
 
 
+@dataclass
+class LocatedError:
+    code: str
+    message: str
+    path: str
+
+
 class DiscordAPIError(EpikCordException):
-    ...
+    def __init__(self, body):
+        self.body = body
+        self.code = body.get("code")
+        self.message = body.get("message")
+        self.errors = body.get("errors")
+        self.errors_list = self.extract_errors(self.errors)
+
+        super().__init__(
+            "\n".join(
+                f"{e.path} - {e.code} - {e.message}"
+                for e in self.errors_list
+            )
+        )
+
+    def extract_errors(self, d, key_path=None):
+        """Get _errors key from the given dictionary."""
+        if key_path is None:
+            key_path = []
+
+        if "_errors" in d:
+            return [
+                LocatedError(**error, path='.'.join(key_path[1:]))
+                for error in d["_errors"]
+            ]
+
+        return [
+            x for k, v in d.items() if isinstance(v, dict)
+            for x in self.extract_errors(v, key_path + [k])
+        ]
 
 
 class InvalidData(EpikCordException):
