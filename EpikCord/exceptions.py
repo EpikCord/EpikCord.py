5bc7b5b37dc74e816eb08f7fa951ccca1b4fad21
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-06-22 19:00:33 +0100

    Current state

--- EpikCord/exceptions.py
+++ EpikCord/exceptions.py
@@ -31,7 +31,34 @@ class MissingCustomId(EpikCordException):
 
 
 class DiscordAPIError(EpikCordException):
-    ...
+    def __init__(self, body):
+        self.body = body
+        self.code = body["code"]
+        self.message = body["message"]
+        self.errors = body.get("errors")
+        self.errors_list = self.extract_errors(self.errors)
+        final_message = ""
+        for error in self.errors_list:
+            final_message += f"{error['path']} - {error['code']} - {error['message']}\n"
+        super().__init__(final_message)
+
+    def extract_errors(self, d, key_path=None):
+        """Get _errors key from the given dictionary."""
+        if key_path is None:
+            key_path = []
+
+        if "_errors" in d:
+            errors = []
+            for error in d["_errors"]:
+                error["path"] = '.'.join(key_path[1:])
+                errors.append(error)
+            return errors
+
+        return [
+            x for k, v in d.items() if isinstance(v, dict)
+            for x in self.extract_errors(v, key_path + [k])
+        ]
+
 
 
 class InvalidData(EpikCordException):
@@ -58,31 +85,27 @@ class DisallowedIntents(EpikCordException):
     ...
 
 
-class t(EpikCordException):
+class Unauthorized401(DiscordAPIError):
     ...
 
 
-class Unauthorized401(EpikCordException):
+class Forbidden403(DiscordAPIError):
     ...
 
 
-class Forbidden403(EpikCordException):
+class NotFound404(DiscordAPIError):
     ...
 
 
-class NotFound404(EpikCordException):
+class MethodNotAllowed405(DiscordAPIError):
     ...
 
 
-class MethodNotAllowed405(EpikCordException):
+class Ratelimited429(DiscordAPIError):
     ...
 
 
-class Ratelimited429(EpikCordException):
-    ...
-
-
-class GateawayUnavailable502(EpikCordException):
+class GateawayUnavailable502(DiscordAPIError):
     ...
 
 
