123e9bdfce2af1d37e85fa07115cc6b41b54beb7
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-07-15 08:21:01 +0100

    Minor changes

--- EpikCord/__main__.py
+++ EpikCord/__main__.py
@@ -1,12 +1,12 @@
 __all__ = ("__version__", "info")
 
-__version__ = "0.5.1"
+__version__ = "0.5.2"
 
 
 def info():
     print(
-        f"Version {__version__} of EpikCord.py, written by EpikHost. "
-        "This is an unstable build and will contain bugs. "
+        f"Version {__version__} of EpikCord.py, written by the organization EpikCord. "
+        "This is an unstable build and may contain bugs. "
         "Please report any bugs to https://github.com/EpikCord/EpikCord.py/issues."
     )
 
