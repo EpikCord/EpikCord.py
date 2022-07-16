8092d4cb648df21ef6dc55ea432107b819a96644
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-07-16 19:34:21 +0800

    Removed start and until

--- EpikCord/ext/tasks.py
+++ EpikCord/ext/tasks.py
@@ -1,6 +1,5 @@
 import datetime
 from typing import Callable, Optional
-from time import time
 import asyncio
 from ..exceptions import TaskFailedError
 
@@ -11,32 +10,19 @@ __all__ = (
 )
 
 
-class TimeParser:
-    @staticmethod
-    def get_time_since_epoch(
-        year: int = 0,
-        month: int = 0,
-        day: int = 0,
-        hour: int = 0,
-        minute: int = 0,
-        seconds: float = 0,
-    ) -> float:
-        date = datetime.datetime(year, month, day, hour, minute, seconds)
 
-        delta = date - _EPOCH
-        return delta.total_seconds()
 
-    @staticmethod
-    def total_seconds(
-        day: int = 0,
-        hour: int = 0,
-        minute: int = 0,
-        seconds: float = 0,
-    ) -> float:
-        hour += day * 24
-        minute += hour * 60
-        seconds += minute * 60
-        return seconds
+@staticmethod
+def total_seconds(
+    day: int = 0,
+    hour: int = 0,
+    minute: int = 0,
+    seconds: float = 0,
+) -> float:
+    hour += day * 24
+    minute += hour * 60
+    seconds += minute * 60
+    return seconds
 
 
 class Tasks:
@@ -47,8 +33,6 @@ class Tasks:
         self,
         task: Callable[..., None],
         interval: Optional[int] = None,
-        start: Optional[float] = None,
-        until: Optional[float] = None,
         **kwargs,
     ):
         """Adds a background task (Tasks that run silently in the background)
@@ -59,18 +43,16 @@ class Tasks:
         You may also define whether to run once or run indefinitely
 
         Args:
-            task (Callable): The function to run on the background
-            interval (Optional[int]): The interval (in seconds) for a delay between when the instance of the task stops and starts again.Defaults to 5.
+            task (Callable): 
+            The function to run on the background
+            interval (Optional[int]): 
+            The interval (in seconds) for a delay between when the instance of the task stops and starts again.Defaults to 5.
             You may use `TimeParser.total_seconds` method
-            instances (Optional[int]): The number of instances to start after delay. Usually unlimited
-            start (Optional[float]): The time to start, you may use `TimeParser.get_time_since_epoch` method to give the seconds since python epoch to this arg
-            until (optional[float]): The time to end, you may use `TimeParser.get_time_since_epoch` method to give the seconds since python epoch to this arg
-
+            instances (Optional[int]): 
+            The number of instances to start after delay. Usually unlimited
 
         """
         kwargs["interval"] = interval
-        kwargs["start"] = start
-        kwargs["until"] = until
 
         async def full_task(client, task, **kwargs):
             async def task_func(interval):
@@ -81,17 +63,11 @@ class Tasks:
 
                 await asyncio.sleep(interval)
 
-            interval = int(kwargs.get("interval", 5))
+            interval = interval if interval <= 0 else 5
             task_start = False
             nb_instances = kwargs.get("limit")
-            if kwargs.get("start") and kwargs.get("until"):  # Start when and do until
-                current_time = time()
-                until = float(kwargs.get("until"))
-                if current_time >= float(kwargs.get("start")) or task_start:
-                    while current_time >= until:
-                        await task_func(client, interval)
 
-            elif nb_instances:
+            if nb_instances:
                 instances = int(nb_instances)
                 finished_instances = 0
                 while instances > finished_instances:
