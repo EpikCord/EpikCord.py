b5db71fcb52f00de6dbab221ee60e18f3963b890
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-07-23 22:11:29 +0800

    MAke setup func

--- EpikCord/__main__.py
+++ EpikCord/__main__.py
@@ -1,16 +1,37 @@
 import argparse
-from ast import parse
+import colorama
+
 
 __all__ = ("__version__", "info")
 
 __version__ = "0.5.2"
 
+_bot_tmplate = """#!/usr/bin/env python
+from EpikCord import Client, Intents
+
+intents = Intents.all()
+
+client: Client = Client([token], intents)
+
+
+@client.event()
+async def ready():
+    print("Ready!")
+# Write commands here
+
+client.login()
+"""
+
+colorama.init(autoreset=True)
 parser = argparse.ArgumentParser()
 parser.add_argument("--version", help="displays the version of the library", action='store_true')
-parser.add_argument("bot")
+#parser.add_argument()
 
+def setup():
+    print(f"{colorama.Back.RED}{colorama.Fore.BLUE}Welcome To Setup!")
+    name_query = input("What is the name of the new bot: ")
 
-
+    ...
 
 def info():
     print(
@@ -19,10 +40,16 @@ def info():
         "Please report any bugs to https://github.com/EpikCord/EpikCord.py/issues."
     )
 
-def parse_args(args:argparse.Namespace):
+def parse_args(args:argparse.ArgumentParser):
+    args = parser.parse_args()
     if args.version:
         info()
+    else:
+        setup()
 
 def main():
-    args = parser.parse_args()
-    parse_args(args)
+    
+    parse_args(parser)
+
+if __name__ == '__main__':
+    main()
