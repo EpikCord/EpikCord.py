8d03421c2dfe5a78fb90bffe51dc967f6d29ef07
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-07-24 00:01:36 +0800

    Commit some stuff to the new CLI tool

--- EpikCord/__main__.py
+++ EpikCord/__main__.py
@@ -1,8 +1,9 @@
 import argparse
+import os
+import sys
 import colorama
+from platform import system as get_parent_os
 
-
-__all__ = ("__version__", "info")
 __version__ = "0.5.2"
 _bot_tmplate = """#!/usr/bin/env python
 from EpikCord import Client, Intents
@@ -19,7 +20,17 @@ async def ready():
 
 client.login()
 """
-win_restricted = (
+if get_parent_os().lower() == "windows":
+    IS_WINDOWS = True
+else:
+    IS_WINDOWS = False
+
+
+class InvalidNameError(Exception):
+    ...
+
+
+win_reserved = (
     "CON",
     "CON1",
     "CON2",
@@ -30,17 +41,18 @@ win_restricted = (
     "CON7",
     "CON8",
     "CON9",
+    "PRN",
+    "NUL",
     "AUX",
-    "AUX1",
-    "AUX2",
-    "AUX3",
-    "AUX4",
-    "CON5",
-    "CON6",
-    "CON7",
-    "CON8",
-    "CON9",
-
+    "LPT1",
+    "LPT2",
+    "LPT3",
+    "LPT4",
+    "LPT5",
+    "LPT6",
+    "LPT7",
+    "LPT8",
+    "LPT9",
 )
 
 parser = argparse.ArgumentParser()
@@ -50,11 +62,49 @@ parser.add_argument(
 colorama.init(autoreset=True)
 
 
-def setup():
-    print(f"{colorama.Back.RED}{colorama.Fore.BLUE}Welcome To Setup!")
-    name_query = input("What is the name of the new bot: ")
+def write_bot(token):
+    if IS_WINDOWS:
+        ...
 
-    ...
+
+def setup():
+    if IS_WINDOWS:
+        print(f"{colorama.Back.GREEN}{colorama.Fore.BLUE}Welcome To Setup!")
+        name_query = input("What is the name of the new bot: ")
+        for name in win_reserved:
+            if name_query.upper() == name:
+                raise InvalidNameError(
+                    "This is a Windows reserved keyword, You are not allowed to create these files"
+                )
+        directory = input(
+            "Which directory to write the bot to?:(Type current to make a bot in the current directory): "
+        )
+        if not os.path.isdir(directory) or directory == "current":
+            dir_question = input(
+                "This directory does not exist. Would you like me to create it?(y/n)"
+            )
+            if not dir_question == "y":
+                sys.exit("OK, Exiting...")
+        print("We need to ask you a few questions, then we will finish the setup")
+        token = input("What is the token for this bot?: ")
+        print("Writing the bot to the current directory")
+        write_bot(token)
+    else:
+        print(f"{colorama.Back.GREEN}{colorama.Fore.BLUE}Welcome To Setup!")
+        name_query = input("What is the name of the new bot: ")
+        directory = input(
+            "Which directory to write the bot to?:(Type current to make a bot in the current directory): "
+        )
+        if not os.path.isdir(directory) or directory == "current":
+            dir_question = input(
+                "This directory does not exist. Would you like me to create it?(y/n)"
+            )
+            if not dir_question == "y":
+                sys.exit("OK, Exiting...")
+        print("We need to ask you a few questions, then we will finish the setup")
+        token = input("What is the token for this bot?: ")
+        print("Writing the bot to the current directory")
+        write_bot(token)
 
 
 def info():
