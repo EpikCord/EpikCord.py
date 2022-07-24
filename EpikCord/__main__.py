ef6c7c6df41d2e5765e6d9cdc32d4503a4d80bf1
Falcon Tech <78836383+nav-github01001@users.noreply.github.com>
2022-07-24 19:29:00 +0800

    delete 50 minutes of hard work

--- EpikCord/__main__.py
+++ EpikCord/__main__.py
@@ -1,137 +1,12 @@
 import argparse
-import os
-import re
-import sys
-import colorama
-from platform import system as get_parent_os
 
-from EpikCord import __version__
-_bot_tmplate = """#!/usr/bin/env python
-from EpikCord import Client, Intents
-from config import token
-class [name]Client(Client):
-
-    def __init__(self):
-        super().__init__()
-
-    #[_section]
-    
-intents = Intents.all()
-
-client: [name]Client = [name]Client(token, intents)
-
-
-@client.event()
-async def ready():
-    print("Ready!")
-# Write commands here
-
-client.login()
-"""
-__gitignore_template = """# Byte-compiled / optimized / DLL files
-__pycache__/
-*.py[cod]
-*$py.class
-# C extensions
-*.so
-# Distribution / packaging
-.Python
-env/
-build/
-develop-eggs/
-dist/
-downloads/
-eggs/
-.eggs/
-lib/
-lib64/
-parts/
-sdist/
-var/
-*.egg-info/
-.installed.cfg
-*.egg
-# Our configuration files
-config.py
-"""
-__config_file_template = """
-token = [token]
-"""
-if get_parent_os().lower() == "windows":
-    IS_WINDOWS = True
-else:
-    IS_WINDOWS = False
-
-
-class InvalidNameError(Exception):
-    ...
-
-
+from .__init__ import  __version__
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "--version", help="displays the version of the library", action="store_true"
+    "-v","--version", help="displays the version of the library", action="store_true"
 )
-parser.add_argument("-n","--new", help="Creates an new bot", action='store_true')
-
-colorama.init(autoreset=True)
-
-def get_question(name_query):
-        directory = input(
-            "Which directory to write the bot to?:(Type . to make a bot in the current directory): "
-        )
-        if not os.path.isdir(directory) or directory != ".":
-            dir_question = input(
-                "This directory does not exist. Would you like me to create it?(y/n)"
-            )
-            if dir_question == "y" :
-                os.makedirs(f"{directory}/{name_query}")
-                directory = f"{directory}/{name_query}"
-            else:
-                print("OK, Exiting...")
-                return
-        elif directory == ".":
-            directory = os.getcwd()
-            os.makedirs(f"{directory}/{name_query}")
-            directory = f"{os.getcwd()}/{name_query}"
-        print("We need to ask you a few questions, then we will finish the setup")
-        token = input("Input Token: ")
-        gitignore_confirm = input("Would you like to add a .gitignore file to the folder(y/n):")
-        section_confirm = input("Add a sections folder?")
-        print("Writing the bot to the current directory")
-        return directory,token, gitignore_confirm,section_confirm
-
-def write_bot(name,folder,token,gitignore,if_sections):
-    if gitignore == 'y':
-        with open(f"{folder}/.gitignore", 'x') as f:
-            f.write(__gitignore_template)
-    if if_sections == 'y':
-        os.makedirs(f"{folder}/sections")
-        print('Sections are not yet supported properly')
-    with open(f"{folder}/{name}.py", 'x') as f:
-        f.write(_bot_tmplate.replace('[name]', name))
-    with open(f"{folder}/config.py", 'x') as f:
-        f.write(__config_file_template.replace('[token]', f"\'{token}\'"))
-
-    
-
-def setup():
-    if IS_WINDOWS:
-        print(f"{colorama.Back.GREEN}{colorama.Fore.BLUE}Welcome To Setup!")
-        name_query = input("What is the name of the new bot: ") or "DefaultBot"
-        
-        if re.fullmatch(r'((AUX|CON|LPT)\d?)|NUL|PRN', name_query.upper()):
-            raise InvalidNameError(
-                    "This is a Windows reserved keyword, You are not allowed to create these files"
-                )
-        directory,token, gitignore_confirm, section_confirm=get_question(name_query)
-        write_bot(name_query,directory,token,gitignore_confirm, section_confirm)
-    else:
-        print(f"{colorama.Back.GREEN}{colorama.Fore.BLUE}Welcome To Setup!")
-        name_query = input("What is the name of the new bot: ") or "DefaultBot"
-        directory,token, gitignore_confirm, section_confirm=get_question(name_query)
-        write_bot(name_query,directory,token,gitignore_confirm, section_confirm)
 
 
 def info():
@@ -146,8 +21,6 @@ def parse_args(args: argparse.ArgumentParser):
     args = parser.parse_args()
     if args.version:
         info()
-    elif args.new:
-        setup()
     else:
         print("Error: Program called with no switches")
         parser.print_help()
