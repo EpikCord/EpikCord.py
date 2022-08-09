43063bb089ec34c2f70ce03177b843c65b202fed
TheUntraceable <ridhwanplayzzz@gmail.com>
2022-08-09 13:23:44 +0100

    Fix circular imports!

--- EpikCord/__init__.py
+++ EpikCord/__init__.py
@@ -10,7 +10,7 @@ import io
 import os
 import re
 import zlib
-from abc import abstractmethod
+from abc import abstractproperty
 from base64 import b64encode
 from collections import defaultdict, deque
 from importlib import import_module
@@ -937,81 +937,6 @@ class File:
         self.fp.close = self._closer
         self._closer()
 
-
-class Messageable:
-    def __init__(self, client, channel_id: str):
-        self.id: str = channel_id
-        self.client = client
-
-    async def fetch_messages(
-        self,
-        *,
-        around: Optional[str] = None,
-        before: Optional[str] = None,
-        after: Optional[str] = None,
-        limit: Optional[int] = None,
-    ) -> List[Message]:
-        response = await self.client.http.get(
-            f"channels/{self.id}/messages",
-            params={"around": around, "before": before, "after": after, "limit": limit},
-        )
-        data = await response.json()
-        return [Message(self.client, message) for message in data]
-
-    async def fetch_message(self, *, message_id: str) -> Message:
-        response = await self.client.http.get(
-            f"channels/{self.id}/messages/{message_id}"
-        )
-        data = await response.json()
-        return Message(self.client, data)
-
-    async def send(
-        self,
-        content: Optional[str] = None,
-        *,
-        embeds: Optional[List[dict]] = None,
-        components=None,
-        tts: Optional[bool] = False,
-        allowed_mentions=None,
-        sticker_ids: Optional[List[str]] = None,
-        attachments: List[File] = None,
-        suppress_embeds: bool = False,
-    ) -> Message:
-        payload = {}
-
-        if content:
-            payload["content"] = content
-
-        if embeds:
-            payload["embeds"] = [embed.to_dict() for embed in embeds]
-
-        if components:
-            payload["components"] = [component.to_dict() for component in components]
-
-        if tts:
-            payload["tts"] = tts
-
-        if allowed_mentions:
-            payload["allowed_mentions"] = allowed_mentions.to_dict()
-
-        if sticker_ids:
-            payload["sticker_ids"] = sticker_ids
-
-        if attachments:
-            payload["attachments"] = [
-                attachment.to_dict() for attachment in attachments
-            ]
-
-        if suppress_embeds:
-            payload["suppress_embeds"] = 1 << 2
-
-        response = await self.client.http.post(
-            f"channels/{self.id}/messages", json=payload
-        )
-        data = await response.json()
-        return Message(self.client, data)
-
-
 class User(Messageable):
     def __init__(self, client, data: dict):
         super().__init__(client, data["id"])
@@ -1287,7 +1212,7 @@ class BaseCommand:
     def is_message_command(self):
         return self.type == 3
 
-    @abstractmethod
+    @abstractproperty
     @property
     def type(self):
         ...
--- EpikCord/channels.py
+++ EpikCord/channels.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 from typing import List, Optional, Union, Dict, TYPE_CHECKING
 import socket
 from aiohttp import ClientWebSocketResponse
+from .thread import Thread
 import struct
 from .close_event_codes import GatewayCECode
 from .exceptions import ClosedWebSocketConnection
@@ -32,7 +33,84 @@ class BaseChannel:
         self.type = data.get("type")
 
 if TYPE_CHECKING:
-    from EpikCord import Message, Messageable, Thread, ThreadMember
+    from EpikCord import Message, File, ThreadMember
+
+class Messageable:
+    def __init__(self, client, channel_id: str):
+        self.id: str = channel_id
+        self.client = client
+
+    async def fetch_messages(
+        self,
+        *,
+        around: Optional[str] = None,
+        before: Optional[str] = None,
+        after: Optional[str] = None,
+        limit: Optional[int] = None,
+    ) -> List[Message]:
+        from EpikCord import Message
+        response = await self.client.http.get(
+            f"channels/{self.id}/messages",
+            params={"around": around, "before": before, "after": after, "limit": limit},
+        )
+        data = await response.json()
+        return [Message(self.client, message) for message in data]
+
+    async def fetch_message(self, *, message_id: str) -> Message:
+        from EpikCord import Message
+        response = await self.client.http.get(
+            f"channels/{self.id}/messages/{message_id}"
+        )
+        data = await response.json()
+        return Message(self.client, data)
+
+    async def send(
+        self,
+        content: Optional[str] = None,
+        *,
+        embeds: Optional[List[dict]] = None,
+        components=None,
+        tts: Optional[bool] = False,
+        allowed_mentions=None,
+        sticker_ids: Optional[List[str]] = None,
+        attachments: List[File] = None,
+        suppress_embeds: bool = False,
+    ) -> Message:
+        from EpikCord import Message
+        payload = {}
+
+        if content:
+            payload["content"] = content
+
+        if embeds:
+            payload["embeds"] = [embed.to_dict() for embed in embeds]
+
+        if components:
+            payload["components"] = [component.to_dict() for component in components]
+
+        if tts:
+            payload["tts"] = tts
+
+        if allowed_mentions:
+            payload["allowed_mentions"] = allowed_mentions.to_dict()
+
+        if sticker_ids:
+            payload["sticker_ids"] = sticker_ids
+
+        if attachments:
+            payload["attachments"] = [
+                attachment.to_dict() for attachment in attachments
+            ]
+
+        if suppress_embeds:
+            payload["suppress_embeds"] = 1 << 2
+
+        response = await self.client.http.post(
+            f"channels/{self.id}/messages", json=payload
+        )
+        data = await response.json()
+        return Message(self.client, data)
+
 
 class GuildChannel(BaseChannel):
     def __init__(self, client, data: dict):
