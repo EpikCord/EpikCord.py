d37a9e748cd5937796e26b4876481e3c71757aca
TheUntraceable <TheUntraceable@TheUntraceable.tech>
2022-08-11 22:00:38 +0100

    Seperate Webhooks

--- EpikCord/__init__.py
+++ EpikCord/__init__.py
@@ -997,41 +997,6 @@ class Integration:
         )
 
 
-class WebhookUser:
-    def __init__(self, data: dict):
-        self.webhook_id: str = data.get("webhook_id")
-        self.username: str = data.get("username")
-        self.avatar: str = data.get("avatar")
-
-
-class Webhook:
-    def __init__(self, client, data: dict):
-        self.client = client
-        self.data = data
-        if data:
-            self.id: str = data.get("id")
-            self.type: str = (
-                "Incoming"
-                if data.get("type") == 1
-                else "Channel Follower"
-                if data.get("type") == 2
-                else "Application"
-            )
-            self.guild_id: Optional[str] = data.get("guild_id")
-            self.channel_id: Optional[str] = data.get("channel_id")
-            self.user: Optional[User] = (
-                User(client, data.get("user")) if data.get("user") else None
-            )
-            self.name: Optional[str] = data.get("name")
-            self.avatar: Optional[str] = data.get("avatar")
-            self.token: Optional[str] = data.get("token")
-            self.application_id: Optional[str] = data.get("application_id")
-            self.source_guild: Optional[PartialGuild] = PartialGuild(
-                data.get("source_guild")
-            )
-            self.url: Optional[str] = data.get("url")
-
-
 class Modal:
     def __init__(self, *, title: str, custom_id: str, components: List[ActionRow]):
         self.title = title
@@ -1622,42 +1587,6 @@ class ClientUser:
         data = await response.json()
         # Reinitialize the class with the new data, the full data.
         self.__init__(data)
-
-
-class SourceChannel:
-    def __init__(self, data: dict):
-        self.id: str = data.get("id")
-        self.name: str = data.get("name")
-
-
-class Webhook:  # Not used for making webhooks.
-    def __init__(self, client, data: dict):
-        self.id: str = data.get("id")
-        self.client = client
-        self.type: int = (
-            "Incoming"
-            if data.get("type") == 1
-            else "Channel Follower"
-            if data.get("type") == 2
-            else "Application"
-        )
-        self.guild_id: Optional[str] = data.get("guild_id")
-        self.channel_id: Optional[str] = data.get("channel_id")
-        self.user: Optional[WebhookUser] = (
-            WebhookUser(data.get("user")) if data.get("user") else None
-        )
-        self.name: Optional[str] = data.get("name")
-        self.avatar: Optional[str] = data.get("avatar")
-        self.token: str = data.get("token")
-        self.application_id: Optional[str] = data.get("application_id")
-        self.source_guild: Optional[PartialGuild] = PartialGuild(
-            data.get("source_guild")
-        )
-        self.source_channel: Optional[SourceChannel] = SourceChannel(
-            data.get("source_channel")
-        )
-        self.url: Optional[str] = data.get("url")
-
 
 class Flag:
     if TYPE_CHECKING:
