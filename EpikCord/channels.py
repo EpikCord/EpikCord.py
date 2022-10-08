e90d9bf4b1146e5f0539a3006e480989e05dd740
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2022-10-08 20:41:25 +0100

    Type channels.py

--- EpikCord/channels.py
+++ EpikCord/channels.py
@@ -22,11 +22,11 @@ class Overwrite:
 
 
 class GuildTextChannel(GuildChannel, Messageable):
-    def __init__(self, client, data: dict):
+    def __init__(self, client, data: discord_typings.TextChannelData):
         super().__init__(client, data)
-        self.topic: str = data["topic"]
+        self.topic: Optional[str] = data.get("topic")
         self.rate_limit_per_user: int = data["rate_limit_per_user"]
-        self.last_message_id: str = data["last_message_id"]
+        self.last_message_id: int = int(data["last_message_id"]) # type: ignore
         self.default_auto_archive_duration: int = data["default_auto_archive_duration"]
 
     async def start_thread(
@@ -136,8 +136,8 @@ class GuildTextChannel(GuildChannel, Messageable):
 
 
 class GuildNewsChannel(GuildTextChannel):
-    def __init__(self, client, data: dict):
-        super().__init__(client, data)
+    def __init__(self, client, data: discord_typings.NewsChannelData):
+        super().__init__(client, data) # type: ignore
         self.default_auto_archive_duration: int = data["default_auto_archive_duration"]
 
     async def follow(self, webhook_channel_id: str):
