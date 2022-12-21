86939b878d488b08f947096a2c0b02c3d607b4ed
TheUntraceable <theuntraceable@theuntraceable.me>
2022-12-22 02:36:53 +0600

    Add client

---
+++ EpikCord/client/client.py
@@ -0,0 +1,7 @@
+from ..flags import Intents
+
+class Client:
+    def __init__(self, token: str, intents: Intents):
+        self.token: str = token
+        self.intents: Intents = intents.value
+        
