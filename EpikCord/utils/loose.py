fde7662e90ddb4effef20afcc908dedc59acff77
TheUntraceable <theuntraceable@theuntraceable.me>
2022-12-24 17:22:34 +0600

    Minimise clear_none_values function

--- EpikCord/utils/loose.py
+++ EpikCord/utils/loose.py
@@ -2,7 +2,4 @@ def clear_none_values(d: dict):
     """
     Clears all the values in a dictionary that are None.
     """
-    for key in list(d.keys()):
-        if d[key] is None:
-            del d[key]
-    return d
+    return {k: v for k, v in d.items() if v is not None}
