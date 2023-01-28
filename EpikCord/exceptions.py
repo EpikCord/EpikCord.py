4cd776a9c9a5782faf6bca1062ee558b57ca61c2
TheUntraceable <73362400+TheUntraceable@users.noreply.github.com>
2023-01-28 20:43:26 +0000

    Remove commented out function

--- EpikCord/exceptions.py
+++ EpikCord/exceptions.py
@@ -26,24 +26,6 @@ class HTTPException(EpikCordException):
             "\n".join(f"{e.path} - {e.code} - {e.message}" for e in self.errors_list)
         )
 
-    # def extract_errors(self, d, key_path=None):
-    #     """Get _errors key from the given dictionary."""
-    #     if key_path is None:
-    #         key_path = []
-
-    #     if d.get("_errors"):
-    #         return [
-    #             LocatedError(**error, path=".".join(key_path[1:]))
-    #             for error in d.get("_errors", [])
-    #         ]
-
-    #     return [
-    #         x
-    #         for k, v in d.items()
-    #         if isinstance(v, dict)
-    #         for x in self.extract_errors(v, key_path + [k])
-    #     ]
-
     def extract_errors(self, d, key_path=None):
         """Get _errors key from the given dictionary."""
         if key_path is None:
