67330611316dc4fb8fbdd12af87ac05b67933bd7
sigmanificient <edhyjox@gmail.com>
2023-01-28 19:10:57 +0100

    :white_check_mark: Add a minimal e2e setup

---
+++ tests/e2e/test_bot_instantiation.py
@@ -0,0 +1,24 @@
+import logging
+import sys
+
+from EpikCord import Client, TokenStore, Intents
+from EpikCord.client import *
+from EpikCord.utils import *
+
+
+logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO)
+
+
+class Bot(Client):
+
+    def __init__(self, token: str):
+        super().__init__(
+            token=TokenStore(token),
+            intents=Intents.all()
+        )
+
+
+def test_bot_instantiation():
+    """Check that instantiating to not raise any errors."""
+    assert Bot(sys.argv.pop())
